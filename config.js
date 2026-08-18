3b8ddae2cd92081d6123ac3f849bd9b01b7a528c
kolorare <silviares@gmail.com>
2026-08-18 15:50:50 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+  layers: 3,            // I 3 layer grafici
+  planeWidth: 0.7145,   // Larghezza scalata sul rapporto 886 / 1240 px
+  planeHeight: 1.0,     // Altezza base unità Y
+  maxDepth: 0.3125,     // Nuova profondità scalata (3.5 cm / 21 cm)
+  openDuration: 1500,   // Durata dell'animazione di apertura (ms)
+  staggerDelay: 140,    // Ritardo tra un layer e l'altro (ms)
+  easing: "easeOutCubic"
+};
