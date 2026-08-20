b0d8d78b1861c04b1faff6cc863e15b7e192a18b
kolorare <silviares@gmail.com>
2026-08-20 17:23:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const config = {
   layers: 3,           // I 3 layer grafici
   planeWidth: 1.0,     // Larghezza base unitaria di MindAR
   planeHeight: 1.4000, // Altezza proporzionata a 15x21 cm (21 / 15)
-  maxDepth: 0.2333     // Profondità scalata (3.5 cm / 15 cm)
+  maxDepth: 0.33       // Nuova profondità maggiorata
 };
